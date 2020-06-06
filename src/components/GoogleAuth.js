71d8b551f30ed36756533ecbcdd615169ed3a01b
Athranesh <athranesh@gmail.com>
2020-06-06 17:51:04 +0300

    Added google sign in and sign out buttons. Completed oauth cycle.

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,18 +1,68 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '171877497873-1fonueo2nev151uf5kikg3g4ne36bfe2.apps.googleusercontent.com',
-        scope: 'email',
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '171877497873-1fonueo2nev151uf5kikg3g4ne36bfe2.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn().catch((err) => {
+      return;
+    });
+  };
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderSignInBtn() {
+    return (
+      <button onClick={this.onSignInClick} className="ui purple google button">
+        <i className="google icon" />
+        Sign In With Google
+      </button>
+    );
+  }
+
+  renderSignOutBtn() {
+    return (
+      <button onClick={this.onSignOutClick} className="ui purple google button">
+        <i className="google icon" />
+        Sign Out
+      </button>
+    );
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return;
+    } else if (this.state.isSignedIn) {
+      return this.renderSignOutBtn();
+    } else {
+      return this.renderSignInBtn();
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
